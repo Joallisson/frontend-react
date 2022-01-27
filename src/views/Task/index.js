ef08e7e2bf6c4e0fd3899b96d87945984d796e1d
Joallisson <joallissons@gmail.com>
2022-01-27 20:37:47 -0300

    Tela de cadastro de tarefas criada

--- src/views/Task/index.js
+++ src/views/Task/index.js
@@ -9,6 +9,9 @@ import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import TypeIcons from '../../utils/typeIcons'
 
+import iconCalendar from '../../Assets/calendar.png'
+import iconClock from '../../Assets/clock.png'
+
 function Task(){
 
     const [lateCount, setLateCount] = useState()
@@ -44,8 +47,37 @@ function Task(){
 
                     <Styles.Input>
                         <span>Título</span>
-                        <input type='text' placeholder="Título da Tarefa..."></input>
+                        <input type='text' placeholder="Título da Tarefa..."/>
                     </Styles.Input>
+
+                    <Styles.TextArea>
+                        <span>Detalhes</span>
+                        <textarea rows={5} placeholder="Detalhes da terefa"/>
+                    </Styles.TextArea>
+
+                    <Styles.Input>
+                        <span className="dataHora">Data</span>
+                        <input type='date' placeholder="Título da Tarefa..."/>
+                        <img src={iconCalendar} alt="Calendário"/>
+                    </Styles.Input>
+
+                    <Styles.Input>
+                        <span className="dataHora">Hora</span>
+                        <input type='time' placeholder="Título da Tarefa..."/>
+                        <img src={iconClock} alt="Relógio"/>
+                    </Styles.Input>
+
+                    <Styles.Options>
+                        <div>
+                            <input type='checkbox'/>
+                            <span>CONCLUÍDO</span>
+                        </div>
+                        <button>EXCLUIR</button>
+                    </Styles.Options>
+
+                    <Styles.Save>
+                        <button>SALVAR</button>
+                    </Styles.Save>
 
                 </Styles.Form>
 
--- src/views/Task/styles.js
+++ src/views/Task/styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -10,6 +9,7 @@ export const Container = styled.div`
 
 export const Form = styled.div`
     width: 50%;
+    margin-bottom: 70px;
 `
 
 export const TypeIcons = styled.div`
@@ -41,16 +41,95 @@ export const Input = styled.div`
     width; 100%;
     display: flex;
     flex-direction: column;
+    margin: 20px 0px;
 
     span{
         color: #707070;
-        margin-bottom: 5;
+        margin-bottom: 5px;
     }
 
     input{
         font-size: 16px;
         padding: 15px;
         border: none;
-        border-bottom: 1px solid #EE6B26;
+        border-bottom: 1px solid #EE6B26; 
+    }
+
+    img{
+        width: 15px;
+        position: relative;
+        bottom: 77px;
+    }
+
+    .dataHora{
+        color: #707070;
+        margin-bottom: 5px;
+        margin-left: 20px;
+    }
+`
+
+export const TextArea = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0px;
+    
+    span{
+        color: #707070;
+        margin: 5px 0;
+    }
+
+    textarea{
+        font-size: 16px;
+        padding: 5px;
+        border: 1px solid #EE6B26;
+        border-radius: 10px;
+    }
+`
+
+export const Options = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    button{
+        font-weight: bold;
+        color: #20295F;
+        border: none;
+        background: none;
+        font-size: 18px;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.7;
+        }
+    }
+
+    div{
+        display: flex;
+        align-items: center;
+        color: #EE6B26;
+        font-weight: bold;
+        font-size: 18px;
+    }
+`
+
+export const Save = styled.div`
+    width: 100%;
+    margin-top: 20px;
+
+    button{
+        width: 100%;
+        background: #EE6B26;
+        border: none;
+        font-size: 20px;
+        color: #FFF;
+        font-weight: bold;
+        padding: 20px;
+        border-radius: 30px;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.7;
+        }
     }
 `
